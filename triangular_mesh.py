7ff495d2fac6cdc7054365bcc776da774323b1c2
Filippo Zonta <zontafil@gmail.com>
2019-09-26 14:59:15 +0300

    Support for computing neighbors of voronoi elements

--- triangular_mesh.py
+++ triangular_mesh.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy
+from scipy.spatial import Voronoi, Delaunay
 
 
 class Mesh:
@@ -8,6 +8,7 @@ class Mesh:
     Attributes:
         delaunay {scipy.spatial.Delaunay} -- delaunay mesh
         voronoi {scipy.spatial.Voronoi} -- voronoi mesh
+        voronoiNeighbors {list} -- list of neighbors for each voronoi region
     """
     def __init__(self, delaunay, voronoi):
         """
@@ -18,6 +19,56 @@ class Mesh:
         """
         self.delaunay = delaunay
         self.voronoi = voronoi
+
+    def buildVoronoiNeighborsIndex(self):
+        """Build An array of neighbors for each voronoi region
+           populate the self.voronoiNeighbors array
+        """
+        self.voronoiNeighbors = [None]*len(self.voronoi.regions)
+        for i, region_i in enumerate(self.voronoi.regions):
+            self.voronoiNeighbors[i] = []
+            for j, region_j in enumerate(self.voronoi.regions):
+                if (i == j):
+                    continue
+                # check if region i and j are neighbors.
+                # Add them to the list in case
+                if self.checkRegionsHaveCommonEdge(region_i, region_j):
+                    if (j not in self.voronoiNeighbors[i]):
+                        self.voronoiNeighbors[i].append(j)
+
+    def findVoronoiRegionsNeighbors(self, regionsIndexes):
+        """Find all the neighbors of a set of voronoi mesh elements (a.k.a regions)
+
+        Arguments:
+            regionsIndexes {list} -- indexes (int) of the region
+        """
+        neighbors = []
+        for i in regionsIndexes:
+            for neighbor_i in self.voronoiNeighbors[i]:
+                if (neighbor_i not in neighbors):
+                    neighbors.append(neighbor_i)
+        return neighbors
+
+    def checkRegionsHaveCommonEdge(self, a, b):
+        """Check if two regions have at least one common edge
+
+        Arguments:
+            a {list} -- region A object
+            b {list} -- region B object
+
+        Returns:
+            True/False
+        """
+        ret = False
+        for i in a:
+            if i == -1:
+                continue
+            for j in b:
+                if j == -1:
+                    continue
+                if i == j:
+                    ret = True
+        return ret
 
 
 def generateMesh(b, nx, ny):
@@ -41,4 +92,8 @@ def generateMesh(b, nx, ny):
             offset = 0 if (i % 2) == 0 else (b/2)
             points = np.append(points, [[i*b, j*b + offset]], 0)
 
-    return Mesh(scipy.spatial.Delaunay(points), scipy.spatial.Voronoi(points))
+    mesh = Mesh(Delaunay(points), Voronoi(points))
+
+    # precompute useful stuff, i.e. voronoi neighbors index
+    mesh.buildVoronoiNeighborsIndex()
+    return mesh
